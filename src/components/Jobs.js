a43c89edd983ab41180553a7211afb5b1668ce49
Wijayaac <wijayaac25@gmail.com>
2021-01-16 04:39:37 +0800

    Adding jobs section

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -5,7 +5,63 @@ import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query)
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  const [value, setValue] = React.useState(0)
+  const { company, position, date, desc } = jobs[value]
+
+  return (
+    <section className="section jobs">
+      <Title title="experience" />
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((job, index) => {
+            return (
+              <button
+                onClick={() => setValue(index)}
+                key={job.strapiId}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {job.company}
+              </button>
+            )
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date} </p>
+          {desc.map(item => {
+            return (
+              <div key={item.id} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{item.name} </p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
+
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        strapiId
+        position
+        date
+        desc {
+          id
+          name
+        }
+        company
+      }
+    }
+  }
+`
 
 export default Jobs
