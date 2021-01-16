6148a05098490145db5cba0d5b9ed646302d52cc
Wijayaac <wijayaac25@gmail.com>
2021-01-16 13:54:46 +0800

    adding blog page and post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,10 +2,41 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
-// ...GatsbyImageSharpFluid
 
-const Blog = () => {
-  return <h2>blog page</h2>
+const Blog = ({
+  data: {
+    allStrapiBlogs: { nodes: blogs },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="blog-page">
+        <Blogs blogs={blogs} title="all article" />
+      </section>
+    </Layout>
+  )
 }
+export const query = graphql`
+  {
+    allStrapiBlogs {
+      nodes {
+        category
+        content
+        date(formatString: "MMM Do, YYYY")
+        id
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        slug
+        title
+        desc
+      }
+    }
+  }
+`
 
 export default Blog
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Blogs from "../components/Blogs"
 export default ({ data }) => {
   const {
     allStrapiProjects: { nodes: projects },
+    allStrapiBlogs: { nodes: blogs },
   } = data
   return (
     <Layout>
@@ -16,6 +17,7 @@ export default ({ data }) => {
       <Services />
       <Jobs />
       <Projects projects={projects} title="featured projects" showLink />
+      <Blogs blogs={blogs} title="related article" showLink />
     </Layout>
   )
 }
@@ -40,6 +42,24 @@ export const query = graphql`
             }
           }
         }
+      }
+    }
+    allStrapiBlogs(sort: { fields: date, order: DESC }, limit: 3) {
+      nodes {
+        category
+        content
+        date(formatString: "MMM Do, YYYY")
+        id
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        slug
+        title
+        desc
       }
     }
   }
