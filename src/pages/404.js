1a36112638b72de4c3002307316733dbedd78639
Wijayaac <wijayaac25@gmail.com>
2021-01-17 13:59:37 +0800

    creating a 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,16 @@ import { Link } from "gatsby"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      <main className="error-page">
+        <h1>oops it's like you come to wrong place</h1>
+        <Link to="/" className="btn">
+          back to home
+        </Link>
+      </main>
+    </Layout>
+  )
 }
 
 export default Error
